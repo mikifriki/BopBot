03fb24689c425500f85f6415cfcaf5e54b3cd7f9
mikifriki <mihkel.liblikman@gmail.com>
2020-03-09 22:22:15 +0200

    Added a quick 8ball function.

---
+++ src/commands/eightball.js
@@ -0,0 +1,13 @@
+let eightBallAnswers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes - definitely',
+	'You may rely on it', 'As I see it, yes', 'Most likely', 'Yes', 'Signs point to yes', 'Reply hazy, try again',
+	' Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Dont count on it', 'My reply is no',
+	'My sources say no', 'Outlook not so good', 'Very doubtful'];
+
+module.exports = async (msg) => {
+	let ranAnswer = eightBallAnswers[Math.floor(Math.random() * eightBallAnswers.length)];
+
+	await msg.channel.send(
+		`${msg.author} ${ranAnswer}`
+	);
+
+};
