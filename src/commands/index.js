b4f4508f63a79d9b869020620d4bd18a664245f2
mikifriki <mihkel.liblikman@gmail.com>
2020-03-13 14:40:14 +0200

    Added cooldown to stop spam.

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,14 +4,18 @@ const time = require('./timeZone');
 const stats = require('./winRateChamp');
 const help = require('./help');
 const status = require('./playerRate');
+const overWatch = require('./overWatchStats');
+const virus = require('./virusStatistic');
+const spamProtection = new Set();
 
-/*const guildID = process.env.GUILD_ID;*/
 const testChannelID = process.env.CHANNEL_ID;
 const botChannelID = process.env.BOTCHANNEL_ID;
 
 const commands = {
 	'8league': randomLeague,
 	'8ball': eightBall,
+	'ow': overWatch,
+	virus,
 	help,
 	time,
 	stats,
@@ -20,12 +24,25 @@ const commands = {
 
 module.exports = async msg => {
 	if (msg.author.bot) return;
-	if (msg.channel.id === testChannelID) {
-		const args = msg.content.split(' ');
-		if (args.length === 0 || args[0].charAt(0) !== '#') return;
-		const command = args.shift().substr(1);
-		if (Object.keys(commands).includes(command)) {
-			commands[command](msg, args);
+	try {
+		if (msg.channel.id === testChannelID) {
+			if (spamProtection.has(msg.author.id)) {
+				msg.channel.send('You better stop ' + msg.author);
+				return;
+			}
+			const args = msg.content.split(' ');
+			if (args.length === 0 || args[0].charAt(0) !== '#') return;
+			const command = args.shift().substr(1);
+			if (Object.keys(commands).includes(command)) {
+				spamProtection.add(msg.author.id);
+				commands[command](msg, args);
+			}
 		}
+
+		setTimeout(function() {
+			spamProtection.delete(msg.author.id);
+		}, 10000);
+	} catch (err) {
+		console.log(err);
 	}
 };
