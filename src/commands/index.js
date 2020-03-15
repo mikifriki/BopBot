d2fa9864a9364c2756bb7ad14224099bb2b91000
Mikifriki <mihkel.liblikman@gmail.com>
2020-03-15 19:04:08 +0200

    Readded env to ingore

--- src/commands/index.js
+++ src/commands/index.js
@@ -27,7 +27,7 @@ const commands = {
 module.exports = async msg => {
 	if (msg.author.bot) return;
 	try {
-		if (msg.channel.id === testChannelID) {
+		if (msg.channel.id === botChannelID) {
 			const args = msg.content.split(' ');
 			if (args.length === 0 || args[0].charAt(0) !== '#') return;
 			const command = args.shift().substr(1);
