728dfc3efd441c18ed68f72420556b8a5732a2b5
mikifriki <mihkel.liblikman@gmail.com>
2020-03-25 02:40:57 +0200

    Reworked virust statistics to use api instead of using web scraping.

--- src/commands/miscCommands/virusStatistic.js
+++ src/commands/miscCommands/virusStatistic.js
@@ -1,55 +1,49 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 let url = '';
-let infectionData = [];
+let infectionData = {};
 
-async function get_virus_statistic () {
-	const html = await axios.get(url);
-	const $ = await cheerio.load(html.data);
-	infectionData = [];
-	try {
-		$('.maincounter-number').each((i, ele) => {
-			if (i <= 4) {
-				infectionData.push(
-					$(ele).find('span').text()
-				);
-			}
+async function get_virus_statistic() {
+	await axios.get(url)
+		.then((response) => {
+			return infectionData = response.data;
+		})
+		.catch(function handleError(error) {
+			console.log(error);
 		});
-		return infectionData;
-
-	} catch (err) {
-		console.log(err);
-	}
 }
 
-module.exports = async (msg) => {
-	url = 'https://www.worldometers.info/coronavirus/';
-	await get_virus_statistic();
+module.exports = async (msg, args) => {
 	try {
+		if (!args.length) {
+			url = 'https://corona.lmao.ninja/all';
+		} else {
+			url = `https://corona.lmao.ninja/countries/${args}`;
+		}
+		await get_virus_statistic();
 		await msg.channel.send({
-			embed: {
-				color: 16773120,
-				title: 'Fresh stats for yall fave toyota corolla virus. :heart_eyes: ',
-				fields: [
-					{
-						name: ':zipper_mouth:**Coronavirus Cases**',
-						value: `**${infectionData[0]}**`
-					},
-					{
-						name: '**:skull_crossbones:Deaths:**',
-						value: `${infectionData[1]}`
-					},
-					{
-						name: '**:mask:**Recovered**',
-						value: `${infectionData[2]}`
-					}],
-				timestamp: new Date(),
-				footer: {
-					text: ':spill: We all finna die'
+				embed: {
+					color: 16773120,
+					title: 'Fresh stats for yall fave toyota corolla virus. :heart_eyes: ',
+					fields: [
+						{
+							name: ':zipper_mouth:**Coronavirus Cases**',
+							value: `**${infectionData.cases.toLocaleString('en')}**`
+						},
+						{
+							name: '**:skull_crossbones:Deaths:**',
+							value: `${infectionData.deaths.toLocaleString('en')}`
+						},
+						{
+							name: '**:mask:**Recovered**',
+							value: `${infectionData.recovered.toLocaleString('en')}`
+						}],
+					timestamp: new Date(),
+					footer: {
+						text: ':spill: We all finna die'
+					}
 				}
 			}
-		}
 		);
 	} catch (err) {
 		console.log(err);
