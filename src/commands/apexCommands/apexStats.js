7564546cbf14e037d15f460db81360b68f78ab40
mikifriki <mihkel.liblikman@gmail.com>
2020-04-23 20:35:02 +0300

    Remade and fixed formatting of apexStats.js

--- src/commands/apexCommands/apexStats.js
+++ src/commands/apexCommands/apexStats.js
@@ -1,33 +1,27 @@
 const axios = require('axios');
 const apiKey = process.env.TRACKER_KEY;
-let apexInfo = {};
-//TRN-Api-Key: 3de2d922-9b88-41bf-b4a7-6ec34f2bd57f
-async function get_ow_data(url) {
-	await axios.get(url, {headers: {'TRN-Api-Key':apiKey} })
-		.then((response) => {
-			return apexInfo = response.data;
-		})
-		.catch(console.log);
-}
+
+const getApexData = async url => await axios.get(url, { headers: { 'TRN-Api-Key': apiKey } })
+	.then((response => {
+		return response.data;
+	}));
 
 module.exports = async (msg, args) => {
 	let url = `https://public-api.tracker.gg/v2/apex/standard/profile/origin/${args}`;
-	await get_ow_data(url);
-	const generalData = apexInfo.data.segments[0].stats;
-	const rank = apexInfo.data.segments[0].stats.rankScore;
 	if (!args.length) return;
-	try {
-		if (url === 'https://public-api.tracker.gg/v2/apex/standard/profile/origin/0sugoidesune') {
-			await msg.channel.send(
-				`Level: ${generalData.level.displayValue}, Kills: ${generalData.kills.displayValue}, Rank Score: ${rank.displayValue}, Stuck in: ${rank.metadata.rankName}, Hotel? Trivago`
-			);
-		} else {
-			await msg.channel.send(
-				`Level: ${generalData.level.displayValue}, Kills: ${generalData.kills.displayValue}, Rank Score: ${rank.displayValue}, Rank: ${rank.metadata.rankName}, Hotel? Trivago`
-			);
-		}
-	} catch (err) {
-		console.log(err);
-	}
+	await getApexData(url)
+		.then((data) => {
+			const generalData = data.data.segments[0].stats;
+			const rank = data.data.segments[0].stats.rankScore;
+			if (url === 'https://public-api.tracker.gg/v2/apex/standard/profile/origin/0sugoidesune') {
+				msg.channel.send(
+					`Level: ${generalData.level.displayValue}, Kills: ${generalData.kills.displayValue}, Rank Score: ${rank.displayValue}, Stuck in: ${rank.metadata.rankName}, Hotel? Trivago`
+				);
+			} else {
+				msg.channel.send(
+					`Level: ${generalData.level.displayValue}, Kills: ${generalData.kills.displayValue}, Rank Score: ${rank.displayValue}, Rank: ${rank.metadata.rankName}, Hotel? Trivago`
+				);
+			}
+		}).catch(console.log);
 };
 
